7d2cd94c15e75c600d0c4bb5e4b1da37c6feac00
Wing Ortiz <wing.ortiz_sd@hotmail.com>
2022-12-14 12:32:18 -0600

    Fisrt commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Bienvenido</h1>')
+})
+
+app.get('/about', (req, res) => {
+    res.send('<h1>About</h1>')
+})
+app.listen(process.env.PORT || 3000);
+console.log(`listening on port ${process.env.PORT || 3000}`);
